fa2663f266b4d7e4fe7b86920403407bbbb46844
bruno miranda <bruno.silva>
2020-10-25 20:33:34 -0300

    feat(DATABASE): [BR] connect to database - mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const routes = require('./src/routes');
+const {
+  APPLICATION_NAME,
+  SERVER_PORT,
+  DATABASE_NAME,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD
+} = process.env;
 
 const app = express();
-const port = process.env.SERVER_PORT;
 
+mongoose.connect(`mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@0.0.0.0:27017/${DATABASE_NAME}?authSource=admin`, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Application available port ${port}`);
+app.listen(SERVER_PORT, () => {
+  console.log(`Application ${APPLICATION_NAME} available on port ${SERVER_PORT}`);
 });
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,31 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
+const Upload = require('./models/Upload');
 
 routes.get('/', (req, res) => {
-  res.json({ message: 'Great start! Now its progress focus'});
+  res.json({ message: 'Great start! Now its progress focus' });
 });
+
+routes.post(
+  '/upload',
+  multer(multerConfig).single('file'),
+  async (req, res) => {
+    const {
+      originalname: name,
+      size,
+      filename: key
+    } = req.file;
+
+    const upload = await Upload.create({
+      name,
+      size,
+      key,
+      url: ''
+    });
+
+    return res.status(201).json(upload);
+  });
 
 module.exports = routes;
