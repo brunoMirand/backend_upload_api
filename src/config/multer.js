6dde74d86744b84f0ba066ca9473d9954d6f589f
bruno miranda <bruno.silva>
2020-11-01 21:37:34 -0300

    feat(UPLOAD-S3): [BR] create configuration to upload files to AWS s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
 const path = require('path');
 const crypto = require('crypto');
+const { STORAGE_TYPE, AWS_BUCKET_NAME } = process.env;
 
 const pathFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
 
-module.exports = {
-  dest: pathFolder,
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, pathFolder);
     },
@@ -16,11 +19,32 @@ module.exports = {
           callback(err);
         }
 
-        const fileName = `${hash.toString('hex')}-${file.originalname}`;
-        callback(null, fileName);
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+        callback(null, file.key);
       });
     },
   }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: AWS_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, callback) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) {
+          callback(err);
+        }
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`;
+        callback(null, fileName);
+      });
+    }
+  })
+};
+
+module.exports = {
+  dest: pathFolder,
+  storage: storageTypes[STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
