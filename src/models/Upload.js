f3724ae28f6855ee1ece1f67e71bfb89de579f96
bruno miranda <bruno.silva>
2020-11-01 21:43:32 -0300

    feat(UPLOAD): [BR] create validation to know how to get the right url, when uploading to AWS s3 or localhost, the same is used when deleting the images

--- src/models/Upload.js
+++ src/models/Upload.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
+const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 
-const Uploadchema = new mongoose.Schema({
+const {
+  BACKEND_UPLOAD_BASE_URL,
+  SERVER_PORT,
+  STORAGE_TYPE,
+  AWS_BUCKET_NAME
+} = process.env
+
+const s3 = new aws.S3();
+
+const UploadSchema = new mongoose.Schema({
   name: String,
   size: Number,
   key: String,
@@ -11,4 +24,23 @@ const Uploadchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('Upload', Uploadchema);
+UploadSchema.pre('save', function() {
+  if (!this.url) {
+    this.url = `${BACKEND_UPLOAD_BASE_URL}:${SERVER_PORT}/files/${this.key}`;
+  }
+});
+
+UploadSchema.pre('remove', function () {
+  if (STORAGE_TYPE === 's3') {
+    return s3.deleteObject({
+      Bucket: AWS_BUCKET_NAME,
+      Key: this.key
+    }).promise()
+  } else {
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key)
+    );
+  }
+})
+
+module.exports = mongoose.model('Upload', UploadSchema);
