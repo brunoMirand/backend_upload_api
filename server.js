af9ee5cc6c4bd29f278fd4037fb706559b36fc3a
bruno miranda <bruno.silva>
2020-11-18 21:06:17 -0300

    feat(CORS): [BR] add cors middleware in express

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./src/routes');
 const path = require('path');
+const cors = require('cors');
 const {
   APPLICATION_NAME,
   SERVER_PORT,
@@ -16,14 +17,7 @@ const {
 
 const app = express();
 
-mongoose.connect(
-  `mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_BASE_URL}:${DATABASE_PORT}/${DATABASE_NAME}?authSource=admin`,
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  }
-);
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
@@ -33,6 +27,18 @@ app.use(
   express.static(path.resolve(__dirname, 'tmp', 'uploads'))
 );
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Application ${APPLICATION_NAME} available on port ${SERVER_PORT}`);
-});
+const run = async () => {
+  await mongoose.connect(
+    `mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_BASE_URL}:${DATABASE_PORT}/${DATABASE_NAME}?authSource=admin`,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  );
+
+  await app.listen(SERVER_PORT, () => {
+    console.log(`Application ${APPLICATION_NAME} available on port ${SERVER_PORT}`);
+  });
+}
+
+run();
